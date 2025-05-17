b67b0757aaeb4b5646c942adadcb5723f7274e6d
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-16 21:32:57 -0300

    feat: criando e estilizando Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Logo, CtaBanner } from './styles'
+
+const Header = () => (
+  <>
+    <Logo src="#" alt="" />
+    <CtaBanner>
+      Viva experiências gastronômicas no conforto da sua casa
+    </CtaBanner>
+  </>
+)
+
+export default Header
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Logo = styled.img`
+  width: 125px;
+`
+
+export const CtaBanner = styled.h2`
+  font-size: 36px;
+  font-weight: bolder;
+`
