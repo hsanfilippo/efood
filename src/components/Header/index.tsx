d5426d9d5de3a7fdc8065771ea1312cae5385048
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-17 13:00:36 -0300

    feat: estilização do Banner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
-import { Logo, CtaBanner } from './styles'
+import { Logo, CtaBanner, Background } from './styles'
+
+import bannerVector from '../../assets/images/banner_vector.svg'
+import logoVector from '../../assets/images/logo_vector.svg'
 
 const Header = () => (
   <>
-    <Logo src="#" alt="" />
-    <CtaBanner>
-      Viva experiências gastronômicas no conforto da sua casa
-    </CtaBanner>
+    <Background style={{ backgroundImage: `url(${bannerVector})` }}>
+      <div className="container">
+        <div className="logo-container">
+          <Logo src={logoVector} alt="" />
+        </div>
+        <CtaBanner>
+          Viva experiências gastronômicas no conforto da sua casa
+        </CtaBanner>
+      </div>
+    </Background>
   </>
 )
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,33 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Background = styled.div`
+  background-size: cover;
+  display: block;
+  height: 384px;
+  padding-top: 64px;
+  padding-bottom: 40px;
+  display: flex;
+  justify-content: center;
+
+  .container {
+    max-width: 539px;
+  }
+
+  .logo-container {
+    display: flex;
+    justify-content: space-around;
+  }
+`
 
 export const Logo = styled.img`
   width: 125px;
+  margin-bottom: 138px;
 `
 
 export const CtaBanner = styled.h2`
   font-size: 36px;
   font-weight: bolder;
+  text-align: center;
+  color: ${cores.coral};
 `
