a560cc9d5b42a0a95415546ff37b98f58a33de01
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-18 00:35:16 -0300

    feat: criando page para Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import Restaurantes from '../../components/Restaurantes'
+
+const Home = () => (
+  <>
+    <Header />
+    <Restaurantes />
+    <Footer />
+  </>
+)
+
+export default Home
