b82febd7401a1c0c3324ce80f957d36c70f96f7c
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-18 00:20:49 -0300

    feat: estruturando footer com styled-components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,24 +1,35 @@
 import { Logo } from '../../styles'
 
+import {
+  FooterContainer,
+  InfoContainer,
+  LogosContainer,
+  SocialMedia
+} from './styles'
+
 import logoVector from '../../assets/images/logo_vector.svg'
 import logoInstagram from '../../assets/images/logo_instagram.svg'
 import logoFacebook from '../../assets/images/logo_facebook.svg'
 import logoTwitter from '../../assets/images/logo_twitter.svg'
 
 const Footer = () => (
-  <footer>
-    <Logo src={logoVector} />
-    <div>
-      <img src={logoInstagram} alt="Instagram" />
-      <img src={logoFacebook} alt="Facebok" />
-      <img src={logoTwitter} alt="Twitter" />
-    </div>
+  <FooterContainer>
+    <InfoContainer>
+      <LogosContainer>
+        <Logo src={logoVector} />
+        <SocialMedia>
+          <img src={logoInstagram} alt="Instagram" />
+          <img src={logoFacebook} alt="Facebok" />
+          <img src={logoTwitter} alt="Twitter" />
+        </SocialMedia>
+      </LogosContainer>
+    </InfoContainer>
     <p>
       A efood é uma plataforma para divulgação de estabelecimentos, a
       responsabilidade pela entrega, qualidade dos produtos é toda do
       estabelecimento contratado.
     </p>
-  </footer>
+  </FooterContainer>
 )
 
 export default Footer
