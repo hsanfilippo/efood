c7f335de11918a310d7ecbb361bd1ef933ec0089
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-18 00:20:33 -0300

    feat: criando styled components de Footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,1 +1,35 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const FooterContainer = styled.footer`
+  background-color: ${cores.bege};
+  height: 298px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+
+  p {
+    font-size: 10px;
+    color: ${cores.coral};
+    text-align: center;
+    max-width: 480px;
+    margin: 0 auto;
+  }
+`
+export const InfoContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`
+
+export const LogosContainer = styled.div`
+  margin-bottom: 80px;
+`
+
+export const SocialMedia = styled.div`
+  margin: 32px auto 0 auto;
+  max-width: 88px;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const TextContainer = styled.div``
