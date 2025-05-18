84b75bfd9629b72f7d79e88a64a6791414198b4d
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-18 11:36:30 -0300

    chore: substituindo por container global

--- src/components/HeaderPerfil/index.tsx
+++ src/components/HeaderPerfil/index.tsx
@@ -5,7 +5,7 @@ import {
   TagRestaurante,
   TitleRestaurante
 } from './styles'
-import { Logo } from '../../styles'
+import { Logo, GlobalContainer } from '../../styles'
 
 import bannerVector from '../../assets/images/banner_vector.svg'
 import logoVector from '../../assets/images/logo_vector.svg'
@@ -15,17 +15,19 @@ const HeaderPerfil = () => (
   <>
     <HeaderContainer>
       <BannerBg style={{ backgroundImage: `url(${bannerVector})` }}>
-        <div className="container">
+        <GlobalContainer className="container">
           <h3>Restaurantes</h3>
           <div>
             <Logo src={logoVector} alt="" />
           </div>
           <h3>0 produto(s) no carrinho</h3>
-        </div>
+        </GlobalContainer>
       </BannerBg>
       <HeroBg style={{ backgroundImage: `url(${heroPerfil})` }}>
-        <TagRestaurante>Italiana</TagRestaurante>
-        <TitleRestaurante>La Dolce Vita Trattoria</TitleRestaurante>
+        <GlobalContainer>
+          <TagRestaurante>Italiana</TagRestaurante>
+          <TitleRestaurante>La Dolce Vita Trattoria</TitleRestaurante>
+        </GlobalContainer>
       </HeroBg>
     </HeaderContainer>
   </>
