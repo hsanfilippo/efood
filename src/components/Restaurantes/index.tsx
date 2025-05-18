6a58828aeeeac770f90d327e18d0e4dfc041e239
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-17 23:35:49 -0300

    feat: modularizando com TagButton e CardRestaurante

--- src/components/Restaurantes/index.tsx
+++ src/components/Restaurantes/index.tsx
@@ -1,13 +1,57 @@
+import CardRestaurante from '../CardRestaurante'
 import { Container, ListaRestaurantes, Restaurante } from './styles'
+
+import thumbSushi from '../../assets/images/thumb_sushi.png'
+import thumbMacarraum from '../../assets/images/thumb_macarraum.png'
 
 const Restaurantes = () => (
   <>
     <Container>
       <ListaRestaurantes>
-        <Restaurante>Card 1</Restaurante>
-        <Restaurante>Card 2</Restaurante>
-        <Restaurante>Card 3</Restaurante>
-        <Restaurante>Card 4</Restaurante>
+        <Restaurante>
+          <CardRestaurante
+            image={thumbSushi}
+            title="Hioki Sushi"
+            rate={4.9}
+            description={
+              'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!'
+            }
+            tags={['Destaque da semana', 'Japonesa']}
+          />
+        </Restaurante>
+        <Restaurante>
+          <CardRestaurante
+            image={thumbMacarraum}
+            title="La Dolce Vita Trattoria"
+            rate={4.6}
+            description={
+              'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!'
+            }
+            tags={['Italiana']}
+          />
+        </Restaurante>
+        <Restaurante>
+          <CardRestaurante
+            image={thumbMacarraum}
+            title="La Dolce Vita Trattoria"
+            rate={4.6}
+            description={
+              'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!'
+            }
+            tags={['Italiana']}
+          />
+        </Restaurante>
+        <Restaurante>
+          <CardRestaurante
+            image={thumbMacarraum}
+            title="La Dolce Vita Trattoria"
+            rate={4.6}
+            description={
+              'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!'
+            }
+            tags={['Italiana']}
+          />
+        </Restaurante>
       </ListaRestaurantes>
     </Container>
   </>
