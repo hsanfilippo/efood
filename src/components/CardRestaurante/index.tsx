4b2a44126ce2b6bfecfe801464ef1c74c41b0467
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-19 14:43:48 -0300

    feat: refactor em CardRestaurante para redirecionar ao perfil e renderizar tag de destaque

--- src/components/CardRestaurante/index.tsx
+++ src/components/CardRestaurante/index.tsx
@@ -1,9 +1,11 @@
 type Props = {
   image: string
   title: string
-  description: string
   rate: number
+  description: string
   type: string
+  destaque?: boolean
+  restaurante: Restaurante
 }
 
 import { Link } from 'react-router-dom'
@@ -17,27 +19,41 @@ import {
   CardContainer,
   TagsContainer
 } from './styles'
+import Restaurante from '../../models/Restaurante'
 
-const CardRestaurante = ({ image, title, description, rate, type }: Props) => (
-  <Card>
-    <img src={image} alt={title} />
-    <TagsContainer>
-      <ButtonTag isButton={false} text={type} />
-    </TagsContainer>
-    <CardContainer>
-      <TitleRateContainer>
-        <h3>{title}</h3>
-        <Rating>
-          <p>{rate}</p>
-          <img src={rateStar} alt="nota" />
-        </Rating>
-      </TitleRateContainer>
-      <Description>{description}</Description>
-      <Link to="/perfil">
-        <ButtonTag isButton text="Saiba mais" />
-      </Link>
-    </CardContainer>
-  </Card>
-)
+const CardRestaurante = ({
+  image,
+  title,
+  rate,
+  description,
+  type,
+  destaque,
+  restaurante
+}: Props) => {
+  return (
+    <Card>
+      <img src={restaurante.capa} alt={restaurante.titulo} />
+      <TagsContainer>
+        {restaurante.destacado && (
+          <ButtonTag isButton={false} text="Destaque da semana" />
+        )}
+        <ButtonTag isButton={false} text={restaurante.tipo} />
+      </TagsContainer>
+      <CardContainer>
+        <TitleRateContainer>
+          <h3>{restaurante.titulo}</h3>
+          <Rating>
+            <p>{restaurante.avaliacao}</p>
+            <img src={rateStar} alt="nota" />
+          </Rating>
+        </TitleRateContainer>
+        <Description>{restaurante.descricao}</Description>
+        <Link to={`perfil/${restaurante.id}`}>
+          <ButtonTag isButton text="Saiba mais" />
+        </Link>
+      </CardContainer>
+    </Card>
+  )
+}
 
 export default CardRestaurante
