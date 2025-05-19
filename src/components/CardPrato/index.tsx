75a402e321428e65dddedf4de544ef4c39c88971
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-19 15:37:42 -0300

    feat: function para igualar o tamanho das descricoes no card

--- src/components/CardPrato/index.tsx
+++ src/components/CardPrato/index.tsx
@@ -2,13 +2,22 @@ import Prato from '../../models/Prato'
 import LargeBtn from '../LargeBtn'
 import { Card, TitlePrato, DescriptionPrato } from './styles'
 
-const CardPrato = ({ image, title, description, id }: Prato) => (
-  <Card>
-    <img src={image} alt={title} />
-    <TitlePrato>{title}</TitlePrato>
-    <DescriptionPrato>{description}</DescriptionPrato>
-    <LargeBtn text="Adicionar ao carrinho" />
-  </Card>
-)
+const CardPrato = ({ image, title, description, id }: Prato) => {
+  const getDescricao = (descricao: string) => {
+    if (descricao.length > 129) {
+      return descricao.slice(0, 129) + '...'
+    }
+    return descricao
+  }
+
+  return (
+    <Card>
+      <img src={image} alt={title} />
+      <TitlePrato>{title}</TitlePrato>
+      <DescriptionPrato>{getDescricao(description)}</DescriptionPrato>
+      <LargeBtn text="Adicionar ao carrinho" />
+    </Card>
+  )
+}
 
 export default CardPrato
