5c104f62b992f753ca8173a3ca8521adc10cf4ab
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-19 14:56:12 -0300

    feat: integrando Perfil com a API

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 import HeaderPerfil from '../../components/HeaderPerfil'
 import Pratos from '../../components/Pratos'
+import Restaurante from '../../models/Restaurante'
 
-const Perfil = () => (
-  <>
-    <HeaderPerfil />
-    <Pratos />
-  </>
-)
+const Perfil = () => {
+  const { id } = useParams()
+
+  const [restaurante, setRestaurante] = useState<Restaurante>()
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
+      .then((res) => res.json())
+      .then((res) => setRestaurante(res))
+  }, [id])
+
+  if (!restaurante) {
+    return <h3>Carregando...</h3>
+  }
+
+  return (
+    <>
+      <HeaderPerfil
+        cover={restaurante.capa}
+        title={restaurante.titulo}
+        type={restaurante.tipo}
+      />
+      <Pratos />
+    </>
+  )
+}
 
 export default Perfil
