54b4ff1e71f00a7fca925a740445f47b3c539838
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 09:26:31 -0300

    feat: criando e estilizando component de carrinho (Cart)

---
+++ src/components/Cart/styles.ts
@@ -0,0 +1,102 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+import fechar from '../../assets/images/fechar.png'
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 0.7;
+  content: '';
+`
+
+export const CartContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: none;
+  justify-content: flex-end;
+  z-index: 1;
+
+  &.is-open {
+    display: flex;
+  }
+`
+
+export const Sidebar = styled.aside`
+  background-color: ${cores.begeClaro};
+  z-index: 1;
+  padding: 40px 16px 0 16px;
+  max-width: 360px;
+  width: 100%;
+
+  button {
+    width: 100%;
+  }
+`
+
+export const Prices = styled.p`
+  font-weight: bold;
+  font-size: 14px;
+  color: ${cores.branco};
+  margin-bottom: 24px;
+
+  span {
+    display: block;
+    font-size: 12px;
+    color: ${cores.branco};
+  }
+`
+
+export const Quantity = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  color: ${cores.branco};
+  margin-top: 32px;
+  margin-bottom: 16px;
+`
+
+export const CartItem = styled.li`
+  display: flex;
+  border-bottom: 1px solid ${cores.branco};
+  padding: 8px 0;
+  position: relative;
+
+  img {
+    height: 80px;
+    width: 80px;
+    object-fit: cover;
+    margin-right: 24px;
+  }
+
+  h3 {
+    color: ${cores.branco};
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  span {
+    display: block;
+    color: ${cores.branco};
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  button {
+    background-image: url(${fechar});
+    width: 16px;
+    height: 16px;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    position: absolute;
+    top: 8px;
+    right: 0px;
+  }
+`
