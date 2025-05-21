dfab3e46273efb7ccc5d9de4b67e453bdd4d2500
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 10:17:16 -0300

    feat: exibicao dinamica da qtd de itens no carrinho

--- src/components/HeaderPerfil/index.tsx
+++ src/components/HeaderPerfil/index.tsx
@@ -10,6 +10,8 @@ import { Logo, GlobalContainer } from '../../styles'
 
 import bannerVector from '../../assets/images/banner_vector.svg'
 import logoVector from '../../assets/images/logo_vector.svg'
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
 type Props = {
   cover: string
@@ -17,26 +19,30 @@ type Props = {
   type: string
 }
 
-const HeaderPerfil = ({ cover, title, type }: Props) => (
-  <>
-    <HeaderContainer>
-      <BannerBg style={{ backgroundImage: `url(${bannerVector})` }}>
-        <GlobalContainer className="container">
-          <Link to="/">Restaurantes</Link>
-          <Link to="/">
-            <Logo src={logoVector} alt="" />
-          </Link>
-          <h3>0 produto(s) no carrinho</h3>
-        </GlobalContainer>
-      </BannerBg>
-      <HeroBg style={{ backgroundImage: `url(${cover})` }}>
-        <GlobalContainer>
-          <TagRestaurante>{type}</TagRestaurante>
-          <TitleRestaurante>{title}</TitleRestaurante>
-        </GlobalContainer>
-      </HeroBg>
-    </HeaderContainer>
-  </>
-)
+const HeaderPerfil = ({ cover, title, type }: Props) => {
+  const { items } = useSelector((state: RootReducer) => state.cart)
+
+  return (
+    <>
+      <HeaderContainer>
+        <BannerBg style={{ backgroundImage: `url(${bannerVector})` }}>
+          <GlobalContainer className="container">
+            <Link to="/">Restaurantes</Link>
+            <Link to="/">
+              <Logo src={logoVector} alt="" />
+            </Link>
+            <h3>{items.length} produto(s) no carrinho</h3>
+          </GlobalContainer>
+        </BannerBg>
+        <HeroBg style={{ backgroundImage: `url(${cover})` }}>
+          <GlobalContainer>
+            <TagRestaurante>{type}</TagRestaurante>
+            <TitleRestaurante>{title}</TitleRestaurante>
+          </GlobalContainer>
+        </HeroBg>
+      </HeaderContainer>
+    </>
+  )
+}
 
 export default HeaderPerfil
