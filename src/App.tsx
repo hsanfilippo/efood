2b1ccddd47319ffaa152e0f98d6ae3b369809b80
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 09:26:41 -0300

    feat: adicionando Cart ao App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Rotas from './routes'
 import Footer from './components/Footer'
+import Cart from './components/Cart'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
@@ -14,6 +15,7 @@ function App() {
           <GlobalCss />
           <Rotas />
           <Footer />
+          <Cart />
         </BrowserRouter>
       </Provider>
     </div>
