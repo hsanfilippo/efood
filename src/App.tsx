6a3d6280552a689a1edd2ec09fff467ac22361f8
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 08:24:58 -0300

    feat: encapsulando App no Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Rotas from './routes'
 import Footer from './components/Footer'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <GlobalCss />
-        <Rotas />
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalCss />
+          <Rotas />
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
