03b2e9051b3a146fba96fb19fa49aed801b78f99
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 09:21:31 -0300

    feat: criando reducer de modal para alterar o estado de exibicao

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import api from '../services/api'
 import CartReducer from './reducers/cart'
+import ModalReducer from './reducers/modal'
 
 export const store = configureStore({
   reducer: {
+    cart: CartReducer,
+    modal: ModalReducer,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) =>
