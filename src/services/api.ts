d4e33bf91e59cc433b2bea713cb2f0692dbb2c8d
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-21 08:22:52 -0300

    feat: configurando store e api

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import Restaurante from '../models/Restaurante'
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://fake-api-tau.vercel.app/api/efood'
+  }),
+  endpoints: (builder) => ({
+    getRestaurantes: builder.query<Restaurante[], void>({
+      query: () => 'restaurantes'
+    }),
+    getRestauranteById: builder.query<Restaurante, string>({
+      query: (id) => `restaurantes/${id}`
+    })
+  })
+})
+
+export const { useGetRestaurantesQuery, useGetRestauranteByIdQuery } = api
+
+export default api
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import api from '../services/api'
+import CartReducer from './reducers/cart'
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
